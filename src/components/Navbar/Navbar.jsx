f6f4a0a89d2ba7105a7c883a2c6f7bebe26fd615
Kevin Koltraka <kevikoltraka157@gmail.com>
2024-12-20 21:43:53 +0100

    changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
-import { FaBars,FaTimes } from "react-icons/fa";
-import zolaha from './../../images/shargan.jpg'
+import { FaBars, FaTimes } from "react-icons/fa";
+import { motion } from 'framer-motion'; // Import Framer Motion
+import zolaha from './../../images/shargan.jpg';
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -11,46 +12,74 @@ const Navbar = () => {
     setIsMobile(!isMobile);
   };
 
+  // Variants for icon animation
+  const iconVariants = {
+    hidden: { rotate: 0, opacity: 0 },
+    visible: { rotate: 180, opacity: 1, transition: { duration: 0.3 } },
+    exit: { rotate: 0, opacity: 0, transition: { duration: 0.3 } },
+  };
+
   return (
     <nav className='main-navbar'>
       <div className='left-navbar'>
         <Link to='/'>
-          <img className='company-logo'  src={zolaha}  alt='logo'/>
-             </Link>
+          <img className='company-logo' src={zolaha} alt='logo' />
+        </Link>
       </div>
+
       <div className='hamburger-menu'>
-      <FaBars className='ham-icon' onClick={handleClick} style={{ display: isMobile ? 'none' : 'block' }} />
-      <FaTimes className='ham-icon' onClick={handleClick} style={{ display: isMobile ? 'block' : 'none' }} />
-            </div>
+        {isMobile ? (
+          <motion.div
+            key="close"
+            variants={iconVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <FaTimes className='ham-icon' onClick={handleClick} />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="open"
+            variants={iconVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <FaBars className='ham-icon' onClick={handleClick} />
+          </motion.div>
+        )}
+      </div>
+
       <div className={isMobile ? 'middle-navbar active' : 'middle-navbar'}>
         <ul className='middle-navbar-list'>
           <li className='link-item'>
-            <Link to='/' className='link'>
+            <Link to='/' className='link' onClick={() => setIsMobile(false)}>
               Home
             </Link>
           </li>
           <li className='link-item'>
-            <Link to='/about' className='link'>
-            Student Journey
+            <Link to='/about' className='link' onClick={() => setIsMobile(false)}>
+              Student Journey
             </Link>
           </li>
           <li className='link-item'>
-            <Link to='/services' className='link'>
+            <Link to='/services' className='link' onClick={() => setIsMobile(false)}>
               Services
             </Link>
           </li>
           <li className='link-item'>
-            <Link to='/event' className='link'>
+            <Link to='/event' className='link' onClick={() => setIsMobile(false)}>
               Events
             </Link>
           </li>
           <li className='link-item'>
-            <Link to='/announcements' className='link'>
+            <Link to='/announcements' className='link' onClick={() => setIsMobile(false)}>
               Announcements
             </Link>
           </li>
           <li className='link-item'>
-            <Link to='/contact' className='link'>
+            <Link to='/contact' className='link' onClick={() => setIsMobile(false)}>
               Contact
             </Link>
           </li>
