9287f7039b2e29264073362fa6041606de896487
Kevin Koltraka <kevikoltraka157@gmail.com>
2025-01-21 15:56:38 +0100

    changes

--- src/components/Partners/Partner.jsx
+++ src/components/Partners/Partner.jsx
@@ -1,11 +1,13 @@
 import { useRef, useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./partner.css";
-import Dots1 from "./BrandImages/9Dots.jpg";
 import Dots2 from "./BrandImages/unimetropolitan.png";
 import Dots3 from "./BrandImages/getex.png";
 import Dots4 from "./BrandImages/flex.png";
 import Dots5 from "./BrandImages/wbu.png";
+import Dots6 from "./BrandImages/coachingLogo.jpg";
+import Dots1 from "./BrandImages/9Dots.jpg";
+
 
 const Partner = () => {
   const scrollRef = useRef(null);
@@ -46,7 +48,7 @@ const Partner = () => {
         animate={{ x: inView ? 0 : "-100%" }}
         transition={{ duration: 1, ease: "easeInOut" }}
       >
-        <motion.img
+       <motion.img
           src={Dots1}
           alt="brand-partners"
           initial={{ opacity: 0 }}
@@ -76,6 +78,13 @@ const Partner = () => {
         />
       <motion.img
           src={Dots5}
+          alt="brand-partners"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: inView ? 1 : 0 }}
+          transition={{ duration: 0.7, delay: 0.9 }}  // Fixed the commas here
+        />
+        <motion.img
+          src={Dots6}
           alt="brand-partners"
           initial={{ opacity: 0 }}
           animate={{ opacity: inView ? 1 : 0 }}
